e93f79cb5965e4380880f53318b9fd28dbecd0b7
unknown <seshdev01012001@gmail.com>
2025-03-26 00:27:00 +0530

    first commit

---
+++ src/main/java/com/AI_Resume_Screnner/AiResumeScrennerApplication.java
@@ -0,0 +1,13 @@
+package com.AI_Resume_Screnner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResumeScrennerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResumeScrennerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AI_Resume_Screnner/AiResumeScrennerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI_Resume_Screnner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeScrennerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
